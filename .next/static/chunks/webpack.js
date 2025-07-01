5a1199e4533a4ad7def0df0d07f69517d4bc1ef0
ElyseK <princesselyse1111@gmail.com>
2025-07-01 17:00:33 -0400

    Envato template added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a9193e0691147bcb"; }
+/******/ 		__webpack_require__.h = function() { return "81808bbbc35a86cd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
